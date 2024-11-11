8cb27862604e0f6980dad63347f8be273c6ab46a
sarah1khalid <sarah1.khalid@torontomu.ca>
2024-11-11 14:42:39 -0500

    update

---
+++ accessibilityfunctions.js
@@ -0,0 +1,14 @@
+// Toggle large text size
+function enlargeText() {
+    document.body.classList.toggle("large-text");
+}
+
+// Toggle high-contrast color scheme
+function toggleColorScheme() {
+    document.body.classList.toggle("high-contrast");
+}
+
+// Display alt-text for images
+function showAltText(text) {
+    alert(text);
+}
